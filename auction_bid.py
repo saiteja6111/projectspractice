90c607f2813c30b29fc2b6ea76d05e7f3283c4ae
saiteja6111 <saitejain8055@gmail.com>
2025-04-12 00:28:19 +0530

    new bid project added

---
+++ auction_bid.py
@@ -0,0 +1,45 @@
+#writing a program on the auction bid
+import os
+
+print("WELCOME TO AUCTION BID")
+auction_dict = {}
+value = True
+
+while value:
+    name = input("enter the name ? ")
+    bid = int(input("Enter your bid amount? $"))
+    auction_dict[name] = bid
+    choice = input("Type \"Yes\" if there are other bidders or \"No\" : ")
+    if choice.lower() == "yes":
+        os.system('clear')
+        value = True
+    elif choice.lower() == "no":
+        value = False
+        os.system('clear')
+
+cost_list=[]
+name_list=[]
+for i in auction_dict.values():
+    cost_list.append(i)
+for j in auction_dict.keys():
+    name_list.append(j)
+
+maximum_bid = max(cost_list)
+
+index_value = cost_list.index(maximum_bid)
+
+print(f"{name_list[index_value]} won the bid of ${maximum_bid}")
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
--- nesting.py
+++ nesting.py
@@ -1,19 +1,23 @@
-#nesting lists in a dictionary
+# #nesting lists in a dictionary
 
-dict = {"name":['saiteja','chaitanya','ramteja','amanullah']}
+# dict = {"name":['saiteja','chaitanya','ramteja','amanullah']}
 
-print(dict['name'][0])
-print(dict['name'][3])
+# print(dict['name'][0])
+# print(dict['name'][3])
 
-nested_list = [1,2,3,4,[0,-1,-2]]
+# nested_list = [1,2,3,4,[0,-1,-2]]
 
-print(nested_list[4][2])
+# print(nested_list[4][2])
 
-#nested list accessing
+# #nested list accessing
 
-travel_log = {"France":{"cities_visited":["paris","lille","dijion"],"total_visited": 12},"Germany":{"cities_visited":["Berlin","Hambug","stuttgrat"],"total_visited":9}}
+# travel_log = {"France":{"cities_visited":["paris","lille","dijion"],"total_visited": 12},"Germany":{"cities_visited":["Berlin","Hambug","stuttgrat"],"total_visited":9}}
 
-print(travel_log["Germany"]["cities_visited"][2])
+# print(travel_log["Germany"]["cities_visited"][2])
 
 
+
+dict = {'saiteja':48,"chiatnaya": 54}
+
+print(dict.get('saiteja'))
 
